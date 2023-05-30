b2f5e3d32e6867f6d98459002f9eb11bb503e0de
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-05-30 14:05:30 -0500

    nots

--- src/roi_to_csv.py
+++ src/roi_to_csv.py
@@ -2,17 +2,16 @@
 #
 # Read FSL .txt timeseries and convert to csv
 
+import numpy
 import pandas
 import sys
 
 roicsv = sys.argv[1]
 meanfsltxt = sys.argv[2]
-fsltxt = sys.argv[3]
 denom = sys.argv[4]
 
 # Output filename
 meanfslcsv = meanfsltxt.replace('.txt', '.csv')
-fslcsv = fsltxt.replace('.txt', '.csv')
 
 # Read ROI labels
 roiinfo = pandas.read_csv(roicsv)
@@ -25,17 +24,10 @@ meandata = pandas.read_csv(
     names=roiinfo.Region,
     dtype=float,
     )
-data = pandas.read_csv(
-    fsltxt, 
-    delim_whitespace=True, 
-    usecols=roiinfo.Label-1,
-    names=roiinfo.Region,
-    dtype=float,
-    )
     
 # Rescale by whole brain mean
 meandata = meandata.applymap(lambda x: x/float(denom))
-data = data.applymap(lambda x: x/float(denom))
+print(meandata)
 
 # Transpose
 region = list(meandata.columns)
